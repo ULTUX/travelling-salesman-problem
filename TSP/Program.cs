3784c81265c8555cb44042870ed86580d43e9fba
WNowak <bulion@zoho.com>
2022-01-25 16:09:55 +0100

    update benchmark parameters
    Signed-off-by: WNowak <bulion@zoho.com>

--- TSP/Program.cs
+++ TSP/Program.cs
@@ -14,23 +14,22 @@ namespace TSP
 
             // new TabuSearch(new ATSPReader("ftv47.atsp").ReadFile(),
             //     1000, SwapMethod.InsertSwap, false).Start();
-            int[] times = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+            int[] times = {500, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500};
             float[] tempMods = {0.7f, 0.75f, 0.80f, 0.85f, 0.9f, 0.95f, 0.99f, 0.999f, 0.9999f, 0.99999f};
             int[] popSizes =
             {
-                100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000
+                5, 10, 20, 40, 50, 100, 200, 400
             };
-            new SecondBenchmark(39, 1000, 10, "ftv47.atsp", null, 
+            new SecondBenchmark(39, 1000, 10, "ftv47.atsp", null,
                 null, CoMethod.OrderedCo, times, tempMods, popSizes, 0.8f, 0.01f);
-
         }
-        }
-
-
-        // var alg = new TabuSearch(new ATSPReader("ftv170.atsp").ReadFile(), 360000, SwapMethod.TwoOperatorSwap);
-        // alg.Start();
-        // var results = alg.GetResults();
-        // Console.WriteLine("Results got from algorithm: best found: {0}", results.costFound);
-        // Graph.PrintSolution(results.solutionFound);
-        // new SimulatedAnnealing(new ATSPReader("br17.atsp").ReadFile(),  5000).Start();
     }
+
+
+    // var alg = new TabuSearch(new ATSPReader("ftv170.atsp").ReadFile(), 360000, SwapMethod.TwoOperatorSwap);
+    // alg.Start();
+    // var results = alg.GetResults();
+    // Console.WriteLine("Results got from algorithm: best found: {0}", results.costFound);
+    // Graph.PrintSolution(results.solutionFound);
+    // new SimulatedAnnealing(new ATSPReader("br17.atsp").ReadFile(),  5000).Start();
+}
--- TSP/Utils/SecondBenchmark.cs
+++ TSP/Utils/SecondBenchmark.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading;
 using TSP.Algorithms;
 
@@ -7,22 +6,23 @@ namespace TSP.Utils
 {
     public class SecondBenchmark
     {
+        private readonly CoMethod? _coMethod;
         private readonly float[] _tempModifiers;
         private readonly int[] _timeConstraints;
         private readonly int timeConstraint;
         private AnnealMethod? _annealMethod;
+        private float _coRate;
+        private float _mtRate;
+        private int[] _popSize;
         private SwapMethod? _swapMethod;
         private Graph graph;
         private string inputFileName;
         private int repeats;
         private int trueCost;
-        private readonly CoMethod? _coMethod;
-        private int[] _popSize;
-        private float _coRate;
-        private float _mtRate;
 
         public SecondBenchmark(int trueCost, int timeConstraint, int repeats, string inputFileName,
-            AnnealMethod? annealMethod, SwapMethod? swapMethod, CoMethod? coMethod, int[] timeConstraints, float[] tempModifiers,
+            AnnealMethod? annealMethod, SwapMethod? swapMethod, CoMethod? coMethod, int[] timeConstraints,
+            float[] tempModifiers,
             int[] popSize, float coRate, float mtRate)
         {
             this.trueCost = trueCost;
@@ -61,93 +61,94 @@ namespace TSP.Utils
             var geneticData = new float[_timeConstraints.Length, 3];
             var geneticDataPopSize = new float[_popSize.Length, 3];
             var it = 0;
-            foreach (var constraint in _timeConstraints)
-            {
-                Console.WriteLine("{0:0.00}% complete",
-                    (float) _timeConstraints.ToList().IndexOf(constraint) / repeats * 100);
-
-                float avgRunTimeTabu = 0;
-                float avgCostTabu = 0;
-
-                float avgRunTimeGenetic = 0;
-                float avgCostGenetic = 0;
-
-                float avgRunTimeAnneal = 0;
-                float avgCostAnneal = 0;
-
-                for (var i = 0; i < 10; i++)
-                {
-                    if (_coMethod != null)
-                    {
-                        var genetic = new GeneticAlgorithm(graph, constraint, _popSize[0], _coRate, _mtRate,
-                            (CoMethod) _coMethod, true);
-                        genetic.Start();
-                        var geneticResult = genetic.GetResults();
-                        avgRunTimeGenetic += Math.Max(0, (float) geneticResult.timeTookMillis);
-                        avgCostGenetic += geneticResult.costFound;
-                    }
-
-                    if (_swapMethod != null)
-                    {
-                        var tabu = new TabuSearch(graph, constraint, (SwapMethod) _swapMethod, true);
-                        tabu.Start();
-                        var tabuResult = tabu.GetResults();
-                        avgRunTimeTabu += Math.Max(0, (float) tabuResult.timeTookMillis);
-                        avgCostTabu += tabuResult.costFound;
-                    }
-
-                    if (_annealMethod == null) continue;
-                    var anneal = new SimulatedAnnealing(graph, constraint, (AnnealMethod) _annealMethod, null, true);
-                    anneal.Start();
-                    var annealResult = anneal.GetResults();
-                    avgRunTimeAnneal += Math.Max(0, (float) annealResult.timeTookMillis);
-                    avgCostAnneal += annealResult.costFound;
-                }
-
-
-                avgRunTimeTabu /= 10;
-                avgCostTabu /= 10;
-
-                avgRunTimeAnneal /= 10;
-                avgCostAnneal /= 10;
-
-                avgRunTimeGenetic /= 10;
-                avgCostGenetic /= 10;
-
-                Console.WriteLine("Tabu: {0}", avgCostTabu);
-                Console.WriteLine("Anneal: {0}", avgCostAnneal);
-                Console.WriteLine("Genetic: {0}", avgCostGenetic);
-
-                tabuData[it, 0] = constraint;
-                tabuData[it, 1] = avgRunTimeTabu;
-                tabuData[it, 2] = avgCostTabu;
-
-                annealData[it, 0] = constraint;
-                annealData[it, 1] = avgRunTimeAnneal;
-                annealData[it, 2] = avgCostAnneal;
-                
-                geneticData[it, 0] = constraint;
-                geneticData[it, 1] = avgRunTimeGenetic;
-                geneticData[it, 2] = avgCostGenetic;
-                it++;
-            }
+            // foreach (var constraint in _timeConstraints)
+            // {
+            //     Console.WriteLine("{0:0.00}% complete",
+            //         (float) _timeConstraints.ToList().IndexOf(constraint) / repeats * 100);
+            //
+            //     float avgRunTimeTabu = 0;
+            //     float avgCostTabu = 0;
+            //
+            //     float avgRunTimeGenetic = 0;
+            //     float avgCostGenetic = 0;
+            //
+            //     float avgRunTimeAnneal = 0;
+            //     float avgCostAnneal = 0;
+            //
+            //     for (var i = 0; i < 10; i++)
+            //     {
+            //         if (_coMethod != null)
+            //         {
+            //             var genetic = new GeneticAlgorithm(graph, constraint, _popSize[0], _coRate, _mtRate,
+            //                 (CoMethod) _coMethod, true);
+            //             genetic.Start();
+            //             var geneticResult = genetic.GetResults();
+            //             avgRunTimeGenetic += Math.Max(0, (float) geneticResult.timeTookMillis);
+            //             avgCostGenetic += geneticResult.costFound;
+            //         }
+            //
+            //         if (_swapMethod != null)
+            //         {
+            //             var tabu = new TabuSearch(graph, constraint, (SwapMethod) _swapMethod, true);
+            //             tabu.Start();
+            //             var tabuResult = tabu.GetResults();
+            //             avgRunTimeTabu += Math.Max(0, (float) tabuResult.timeTookMillis);
+            //             avgCostTabu += tabuResult.costFound;
+            //         }
+            //
+            //         if (_annealMethod == null) continue;
+            //         var anneal = new SimulatedAnnealing(graph, constraint, (AnnealMethod) _annealMethod, null, true);
+            //         anneal.Start();
+            //         var annealResult = anneal.GetResults();
+            //         avgRunTimeAnneal += Math.Max(0, (float) annealResult.timeTookMillis);
+            //         avgCostAnneal += annealResult.costFound;
+            //     }
+            //
+            //
+            //     avgRunTimeTabu /= 10;
+            //     avgCostTabu /= 10;
+            //
+            //     avgRunTimeAnneal /= 10;
+            //     avgCostAnneal /= 10;
+            //
+            //     avgRunTimeGenetic /= 10;
+            //     avgCostGenetic /= 10;
+            //
+            //     Console.WriteLine("Tabu: {0}", avgCostTabu);
+            //     Console.WriteLine("Anneal: {0}", avgCostAnneal);
+            //     Console.WriteLine("Genetic: {0}", avgCostGenetic);
+            //
+            //     tabuData[it, 0] = constraint;
+            //     tabuData[it, 1] = avgRunTimeTabu;
+            //     tabuData[it, 2] = avgCostTabu;
+            //
+            //     annealData[it, 0] = constraint;
+            //     annealData[it, 1] = avgRunTimeAnneal;
+            //     annealData[it, 2] = avgCostAnneal;
+            //     
+            //     geneticData[it, 0] = constraint;
+            //     geneticData[it, 1] = avgRunTimeGenetic;
+            //     geneticData[it, 2] = avgCostGenetic;
+            //     it++;
+            // }
 
             it = 0;
-            
-            
+
+
             foreach (var popSize in _popSize)
             {
                 float avgRunTime = 0;
                 float avgCost = 0;
                 for (var i = 0; i < 10; i++)
                 {
-                    var genetic = new GeneticAlgorithm(graph, timeConstraint, popSize, 0.8f, 0.01f, CoMethod.OrderedCo, false);
+                    var genetic = new GeneticAlgorithm(graph, timeConstraint, popSize, 0.8f, 0.01f, CoMethod.OrderedCo,
+                        true);
                     genetic.Start();
                     var geneticResult = genetic.GetResults();
                     avgRunTime += (float) geneticResult.timeTookMillis;
                     avgCost += geneticResult.costFound;
                 }
-            
+
                 avgRunTime /= 10;
                 avgCost /= 10;
                 geneticDataPopSize[it, 0] = popSize;
